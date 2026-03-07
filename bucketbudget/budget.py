466afa010457055a5b53c36a7db6bea10ce463f2
Jakob <jakobfred1999@gmail.com>
2026-03-07 18:11:11 +1300

    Add view and create methods to see budgets

--- bucketbudget/budget.py
+++ bucketbudget/budget.py
@@ -5,13 +5,11 @@ from flask import redirect
 from flask import render_template
 from flask import request
 from flask import url_for
-# from flask import send_file
 from flask import session
 from werkzeug.exceptions import abort
 
-# from io import BytesIO
-
-# import json
+from bucketbudget.auth import login_required
+from bucketbudget.db import get_db
 
 bp = Blueprint("budget", __name__)
 
@@ -19,25 +17,44 @@ bp = Blueprint("budget", __name__)
 @bp.route("/")
 def index():
     """Dislay an empty budget."""
+    print(f"Attempting to get session id: {session['user_id']}")
     return render_template("budget/index.html")
 
-# This is a test function
-# @bp.route("/download")
-# def create_and_download_json_file():
-#     """Download a JSON file."""
-#     file_name = f'{session['cookie']}-budget.json'
-#     file = open(file_name, "rb")    
-#     return send_file(file, download_name="budget.json", as_attachment=True)
+@bp.route('/budgets')
+@login_required
+def view_all_budgets():
+    db = get_db()
+    user_id = session.get('user_id')
+    if user_id is not None:
+        budgets = db.execute(
+                'SELECT * FROM budget WHERE owner_id is ?', (user_id,)
+            ).fetchall()
+    else:
+        budgets = None
+    return render_template('budget/view_all.html', budgets=budgets)
 
-# def create_test_json_file():
-#     data = {
-#         "Income": {
-#             "Person1": 100,
-#             "Person2": 100,
-#             "SideHustle": 25,
-#         },
-#     }
 
-#     file = open(f'{session['cookie']}-budget.json', 'w')
-#     file.write(json.dumps(data, indent=4))
-#     file.close()
+@bp.route('/budget/create', methods=('GET', 'POST'))
+@login_required
+def create_budget():
+    if request.method == 'POST':
+        title = request.form['title']
+        
+        error = None
+        
+        if not title:
+            error = 'Title is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO budget (owner_id, title)'
+                'VALUES (?, ?)',
+                (g.user['id'], title),
+            )
+            db.commit()
+            return redirect(url_for('index'))
+        
+    return render_template('budget/create.html')
